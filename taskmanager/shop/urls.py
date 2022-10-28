255738012caa5db770a76205d3ac9b0cb88d6de7
juliamigay <juliamigay@gmail.com>
2022-10-28 13:24:15 +0300

    Initial commit

---
+++ taskmanager/shop/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from .import views
+
+app_name = 'taskmanager'
+
+urlpatterns = [
+
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list,
+         name='product_list_by_category'
+         ),
+    path('<int:id>/<slug:slug>', views.product_detail,
+         name='product'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ taskmanager/taskmanager/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('admin/', admin.site.urls),
+    path('main/', include('main.urls')),
+    path('', include('shop.urls', namespace='shop')),
+
+
+    #path('user/', include('users.urls'))
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
